e624c115deddecf3ce704d2db5e201db30d81424
Gabriel Camargos <86752686+Gabrielcamargos28@users.noreply.github.com>
2023-09-13 16:50:06 -0300

    HotFlix 'precicion' e 'scale'

--- src/main/java/me/dio/santanderbootcamp/domain/Account.java
+++ src/main/java/me/dio/santanderbootcamp/domain/Account.java
@@ -13,9 +13,9 @@ public class Account {
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(nullable = false, scale = 13,precision = 2)
+    @Column(nullable = false, scale = 2,precision = 13)
     private BigDecimal balance;
-    @Column(name = "additional_limit",scale = 13,precision = 2)
+    @Column(name = "additional_limit",scale = 2,precision = 13)
     private BigDecimal limit;
 
     public Long getId_account() {
