6969834d468d3f777becf48dfeb35863ece35284
Gabriel Camargos <86752686+Gabrielcamargos28@users.noreply.github.com>
2023-09-14 15:22:45 -0300

    deploy teste 2

--- src/main/java/me/dio/santanderbootcamp/SantanderBootcampApplication.java
+++ src/main/java/me/dio/santanderbootcamp/SantanderBootcampApplication.java
@@ -1,8 +1,10 @@
 package me.dio.santanderbootcamp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "default server url")})
 @SpringBootApplication
 public class SantanderBootcampApplication {
 
